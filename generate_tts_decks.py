e8bdb18e2fcd19673caf86740519401245ae04de
jurgen <juergen.mitsch@gmx.de>
2025-04-24 19:56:34 +0100

    added folder parsing functionality

--- generate_tts_decks.py
+++ generate_tts_decks.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # get set code from config
     set_code = config.get("SET_CODE")
     # get output directory from config
-    img_out_dir = f'{config.get("OUTPUT_ROOT_DIR")}/{set_code}'
+    img_dir = f'{config.get("OUTPUT_ROOT_DIR")}/{set_code}'
 
     # check if set image files need to be downloaded
     if config.get("SOURCE_IMAGES") == 'True':
@@ -19,12 +19,9 @@ if __name__ == "__main__":
         # parse Scryfall for set code and compile card information
         list_of_set_cards = parse_set(root_url, set_code)
         # download card images and store to output directory
-        download_card_images_by_parsing_dict(set_dict=list_of_set_cards, output_dir=img_out_dir)
+        download_card_images_by_parsing_dict(set_dict=list_of_set_cards, output_dir=img_dir)
 
-    loaded_images = read_image_files_from_folder(folder_path=config.get('MONTAGE_IMAGE_INPUT_DIR'))
-
-    generate_image_montage(
-        images=loaded_images,
-        output_folder=config.get('MONTAGE_IMAGE_OUTPUT_DIR'),
-        output_filename=set_code
-    )
+    # check if montage generation needed
+    if config.get("GENERATE_MONTAGE") == 'True':
+        # parse set image folders
+        parse_set_image_folders(source_dir=img_dir, set_code=set_code)
