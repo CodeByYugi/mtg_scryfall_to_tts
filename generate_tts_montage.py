c07ef8d51ed1eaf4b2370c0bff80cf3cb1c55d17
jurgen <juergen.mitsch@gmx.de>
2025-04-24 19:42:11 +0100

    new function to chunk image list into montage chunks

--- generate_tts_montage.py
+++ generate_tts_montage.py
@@ -1,4 +1,4 @@
-import os
+from functools import reduce
 import imageio.v3 as iio
 from skimage.util import montage
 from pathlib import Path
@@ -28,7 +28,7 @@ def generate_image_montage(
         images: list, output_folder: str, output_filename: str,
         montage_grid: tuple=(7,10), montage_channel_axis: int=3
     ) -> None:
-    """Function that combines images from list into a single montage.
+    """Function that combines images from list into montages of a given max grid size.
 
     Parameters
     ------
@@ -38,5 +38,11 @@ def generate_image_montage(
     montage_grid (tuple) -- tuple of (n_rows, n_cols) for montage image (default: (7, 10))
     montage_channel_axis (int) -- axis for colour information needed by scikit.util.montage (default: 3)
     """
-    image_montage = montage(images, grid_shape=montage_grid, channel_axis=montage_channel_axis)
-    iio.imwrite(f'{output_folder}/{output_filename}.jpg', image_montage)
+
+    max_image_count = reduce(lambda nrows, ncols: nrows*ncols, montage_grid)
+
+    chunked_images = [images[i:i + max_image_count] for i in range(0, len(images), max_image_count)]
+
+    for chunk_num, chunk_images in enumerate(chunked_images):
+        image_montage = montage(chunk_images, grid_shape=montage_grid, channel_axis=montage_channel_axis)
+        iio.imwrite(f'{output_folder}/{chunk_num}_{output_filename}.jpg', image_montage)
